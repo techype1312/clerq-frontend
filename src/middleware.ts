32f277d905316834739a6a4b8d2c9a1902383534
Aaryansh Bhardwaj <aaryanshsharma8@gmail.com>
2024-07-20 01:03:11 +0530

    added supabase in utils and middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,49 @@
-import { NextRequest, NextResponse } from "next/server";
+import { type NextRequest } from "next/server";
+import { updateSession } from "@/utils/supabase/middleware";
 
-export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
-  const jwtToken = request.cookies.get("jwtToken")?.value;
-  console.log(jwtToken, pathname, "middleware");
-  if (pathname.startsWith("/dashboard") && !jwtToken && pathname != "/") {
-    return NextResponse.redirect(new URL("/", request.url));
-  } else if ((pathname === "/signup" || pathname === "/signin") && jwtToken) {
-    return NextResponse.redirect(new URL("/dashboard", request.url));
-  }
-  return NextResponse.next();
+export async function middleware(request: NextRequest) {
+  return await updateSession(request);
 }
 
 export const config = {
-  matcher: ["/", "/dashboard/:path*", "/signin", "/signup"],
+  matcher: [
+    // "/",
+    "/signin",
+    "/dashboard/:path*",
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
 };
+
+// import { NextRequest, NextResponse } from "next/server";
+
+// export async function middleware(request: NextRequest) {
+//   const { pathname } = request.nextUrl;
+//   const jwtToken = request.cookies.get("jwtToken")?.value;
+//   const isUserOnboard = request.cookies.get("isUserOnboard")?.value;
+//   if (
+//     jwtToken &&
+//     isUserOnboard === "false" &&
+//     pathname !== "/dashboard/verify-user"
+//     // !pathname.startsWith("/dashboard/verify-")
+//   ) {
+//     return NextResponse.redirect(
+//       new URL("/dashboard/verify-user", request.url)
+//     );
+//   } else if (
+//     jwtToken &&
+//     isUserOnboard === "true" &&
+//     pathname === "/dashboard/verify-user"
+//   ) {
+//     return NextResponse.redirect(new URL("/dashboard", request.url));
+//   }
+//   if (pathname.startsWith("/dashboard") && !jwtToken && pathname != "/") {
+//     return NextResponse.redirect(new URL("/", request.url));
+//   } else if ((pathname === "/signup" || pathname === "/signin") && jwtToken) {
+//     return NextResponse.redirect(new URL("/dashboard", request.url));
+//   }
+//   return NextResponse.next();
+// }
+
+// export const config = {
+//   matcher: ["/", "/dashboard/:path*", "/signin", "/signup"],
+// };
