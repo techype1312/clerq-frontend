c092dd411fb314997fe357b39fd614cc59ed1015
Aaryansh Bhardwaj <aaryanshsharma8@gmail.com>
2024-07-26 22:52:08 +0530

    feat: Added Material Icon as a generalComponent

---
+++ src/components/generalComponents/MaterialSymbol/SymbolIcon.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { MaterialSymbol } from "react-material-symbols";
+import { SymbolCodepoints } from "react-material-symbols";
+
+const SymbolIcon = ({ icon, color, size }: { icon: SymbolCodepoints, color?: string, size?: number }) => {
+  return <MaterialSymbol icon={icon} weight={300} size={size ?? 24} color={color ?? 'black'} />;
+};
+
+export default SymbolIcon;
