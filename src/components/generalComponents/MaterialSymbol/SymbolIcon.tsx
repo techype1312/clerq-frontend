b8d1d46534baf7695e051fc3f63ac90680d12e01
Aaryansh Bhardwaj <aaryanshsharma8@gmail.com>
2024-07-30 01:00:38 +0530

    update: base color changed for icons

--- src/components/generalComponents/MaterialSymbol/SymbolIcon.tsx
+++ src/components/generalComponents/MaterialSymbol/SymbolIcon.tsx
@@ -3,7 +3,7 @@ import { MaterialSymbol } from "react-material-symbols";
 import { SymbolCodepoints } from "react-material-symbols";
 
 const SymbolIcon = ({ icon, color, size }: { icon: SymbolCodepoints, color?: string, size?: number }) => {
-  return <MaterialSymbol icon={icon} weight={300} size={size ?? 24} color={color ?? 'black'} />;
+  return <MaterialSymbol icon={icon} weight={300} size={size ?? 24} color={color ?? '#535460'} />;
 };
 
 export default SymbolIcon;
