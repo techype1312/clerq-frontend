bc1c5b55f0332080f26df1cbdf4bb4b0b8531d40
Aaryansh Bhardwaj <aaryanshsharma8@gmail.com>
2024-07-31 00:48:12 +0530

    Update: plaid img config in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "plaid-merchant-logos.plaid.com",
+        port: "",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
