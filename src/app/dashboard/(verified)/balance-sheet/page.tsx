d98fb9f40eb91b29159ca5c240cf140c22395d1f
Aaryansh Bhardwaj <aaryanshsharma8@gmail.com>
2024-08-01 17:11:39 +0530

    Update: added more customization to headerCard and added balance sheet page

---
+++ src/app/dashboard/(verified)/balance-sheet/page.tsx
@@ -0,0 +1,53 @@
+'use client';
+import DateRangeDropdownSelect from "@/components/generalComponents/DateRangeDropdownSelect";
+import HeaderCard from "@/components/generalComponents/HeaderCard";
+import { cardDetails, dateRangeType } from "@/types/general";
+import { generateDateRange } from "@/utils/utils";
+import React, { useEffect, useState } from "react";
+
+const Page = () => {
+  const dateRange = generateDateRange();
+  const [selectedDateRange, setSelectedDateRange] = useState<dateRangeType>(
+    dateRange[0]
+  );
+  const [selectedDateRangeIndex, setSelectedDateRangeIndex] = useState(0);
+  const [cardDetails, setCardDetails] = useState<cardDetails>({
+    dateRange: dateRange[0],
+    title: "Balance sheet",
+    download:
+      "https://gseijrhbhurcrpgbxrgt.supabase.co/storage/v1/object/sign/Test%20bucket/sample.pdf?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJUZXN0IGJ1Y2tldC9zYW1wbGUucGRmIiwiaWF0IjoxNzIyNDMwNzgxLCJleHAiOjE3MjMwMzU1ODF9.DJSWsvWgcN6VPhR1Tc59nlHu1CcVqCVUDAv2Y5w4RqE&t=2024-07-31T12%3A59%3A41.813Z",
+    leftText: { title: "Net profit", value: -500.0 },
+    centerText: { title: "Gross profit", value: 162500.123 },
+    rightText: { title: "Operating expenses", value: 162500.123 },
+  });
+  useEffect(() => {
+    setCardDetails({
+      dateRange: selectedDateRange,
+      title: "Balance sheet",
+      download:
+        "https://gseijrhbhurcrpgbxrgt.supabase.co/storage/v1/object/sign/Test%20bucket/sample.pdf?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJUZXN0IGJ1Y2tldC9zYW1wbGUucGRmIiwiaWF0IjoxNzIyNDMwNzgxLCJleHAiOjE3MjMwMzU1ODF9.DJSWsvWgcN6VPhR1Tc59nlHu1CcVqCVUDAv2Y5w4RqE&t=2024-07-31T12%3A59%3A41.813Z",
+      leftText: { title: "Assets", value: 500.0 },
+      centerText: { title: "Liabilities", value: 502.123 },
+      rightText: { title: "Equity", value: 500.123 },
+    });
+  }, [selectedDateRange]);
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex justify-between">
+        <h1 className="text-primary text-2xl font-medium ml-1">
+          Balance sheet
+        </h1>
+        <DateRangeDropdownSelect
+          dateRange={dateRange}
+          selectedDateRange={selectedDateRange}
+          setSelectedDateRange={setSelectedDateRange}
+          setSelectedDateRangeIndex={setSelectedDateRangeIndex}
+          selectedDateRangeIndex={selectedDateRangeIndex}
+        />
+      </div>
+      <HeaderCard cardDetails={cardDetails} />
+    </div>
+  );
+};
+
+export default Page;
--- src/components/generalComponents/HeaderCard.tsx
+++ src/components/generalComponents/HeaderCard.tsx
@@ -1,21 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Card, CardContent, CardDescription, CardHeader } from "../ui/card";
 import { cardDetails, textType } from "@/types/general";
 import { DownloadButton } from "./DownloadButton";
+import { formatDateRange } from "@/utils/utils";
 
 const HeaderCard = ({ cardDetails }: { cardDetails: cardDetails }) => {
+  const [formattedDateRange, setFormattedDateRange] = React.useState<string>();
+  useEffect(() => {
+    setFormattedDateRange(
+      formatDateRange(
+        cardDetails.dateRange.startDate,
+        cardDetails.dateRange.endDate
+      )
+    );
+  }, [cardDetails.dateRange.startDate, cardDetails.dateRange.endDate]);
   return (
     <Card>
       <CardHeader className="flex flex-row items-center justify-between ">
-        <p>
-          From {cardDetails.dateRange.startDate} to{" "}
-          {cardDetails.dateRange.endDate}
-        </p>
+        {!cardDetails.title && (
+          <p className="text-sm">
+            From {cardDetails.dateRange.startDate} to{" "}
+            {cardDetails.dateRange.endDate}
+          </p>
+        )}
+        {cardDetails.title && (
+          <div className="flex flex-col gap-2">
+            <p className="text-sm text-muted">From {formattedDateRange}</p>
+            <h1 className="text-primary text-xl">{cardDetails.title}</h1>
+          </div>
+        )}
         <DownloadButton downloadLink={cardDetails.download} />
       </CardHeader>
       <CardContent className="mt-8 flex">
         <ExpenseModule expense={cardDetails.leftText} />
-        <div className="ml-auto flex gap-4">
+        <div className="ml-auto flex gap-8">
           <ExpenseModule expense={cardDetails.centerText} />
           <ExpenseModule expense={cardDetails.rightText} />
         </div>
@@ -28,7 +46,7 @@ const ExpenseModule = ({ expense }: { expense: textType }) => {
   return (
     <div className="flex flex-col">
       <h6 className="text-primary text-sm">{expense.title}</h6>
-      <p className="text-background-primary text-3.5xl">
+      <p className="text-background-primary text-[32px]">
         {expense.value < 0 && "-"}${Math.abs(expense.value).toFixed(2)}
       </p>
     </div>
