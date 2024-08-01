778643d05484ffc886b414eeba025a39b2544823
Anurag Patel <anuragpatel.optnio@gmail.com>
2024-08-02 02:38:30 +0530

    feat: settings pages setup

---
+++ src/app/dashboard/(verified)/settings/company-profile/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className="flex gap-24 flex-row">
+      <h1
+        style={{
+          fontSize: "24px",
+          fontWeight: "500",
+          lineHeight: "28.8px",
+          textAlign: "left",
+          color: "#1E1E2A",
+        }}
+      >
+        Company Profile
+      </h1>
+    </div>
+  );
+};
+
+export default Page;
