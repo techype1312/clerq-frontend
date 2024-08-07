614d52c85e3cde580c57e600250cf56ca38fbec7
Aaryansh Bhardwaj <aaryanshsharma8@gmail.com>
2024-08-07 23:43:51 +0530

    updated server url

--- config.ts
+++ config.ts
@@ -1,4 +1,5 @@
 export const Servers = {
   // LiveServer: "http://localhost:3003/functions/v1/",
- LiveServer: "https://gseijrhbhurcrpgbxrgt.supabase.co/functions/v1/",
+//  LiveServer: "https://gseijrhbhurcrpgbxrgt.supabase.co/functions/v1/",
+ LiveServer: "http://34.213.177.104:3000/api/v1/",
 };
