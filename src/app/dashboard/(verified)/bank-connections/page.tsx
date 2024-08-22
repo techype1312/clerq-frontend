cf10f9fe258f86c55c5aabf65b08dd5db4d4ef85
Aaryansh Bhardwaj <aaryanshsharma8@gmail.com>
2024-08-23 00:49:54 +0530

    bank connections

--- src/app/dashboard/(verified)/bank-connections/page.tsx
+++ src/app/dashboard/(verified)/bank-connections/page.tsx
@@ -7,6 +7,7 @@ import BankingApis from "@/actions/apis/BankingApis";
 import AccountsTable from "@/components/dashboard/bankAccounts/AccountsTable";
 import { useCompanySessionContext } from "@/context/CompanySession";
 import { ErrorProps } from "@/types/general";
+import { useMainContext } from "@/context/Main";
 
 const Page = () => {
   const { currentUcrm } = useCompanySessionContext();
@@ -40,11 +41,15 @@ const Page = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currentUcrm?.company?.id]);
 
+  const {windowWidth} = useMainContext();
+
   return (
     <div className="flex gap-24 flex-row justify-center">
       <div className="w-full lg:max-w-[950px]">
         <div className="flex flex-col gap-4">
+        {windowWidth > 767 && (
           <h1 className="text-primary text-2xl font-medium">Bank Accounts</h1>
+          )}
           <div className="flex flex-col gap-2 mb-4 border-b border-[#F1F1F4]">
             <h6 className="text-primary">Linked bank accounts</h6>
             <p className="text-muted">
