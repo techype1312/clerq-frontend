67ef597c13c829e65514ffd7b1970788d4066f73
Anurag Patel <anuragpatel.optnio@gmail.com>
2024-08-24 03:50:29 +0530

    fix: change api server url

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 export const Servers = {
-  LiveServer: process.env.API_SERVER_BASE_URL || "https://34.213.177.104:3000/api/v1/",
+  LiveServer: process.env.API_SERVER_BASE_URL || "https://stagingapi.joinotto.com/api/v1/",
   GoogleAPIkey: process.env.GOOGLE_API_KEY || "AIzaSyDOQ7N0NgZt8OFcioja-gHnX5hKjk-Su_8",
   AppEnv: process.env.APP_ENV || 'development'
 };
