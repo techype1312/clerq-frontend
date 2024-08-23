83c2171dc332c085a77e7f4d3eb7085ae37bcf43
Anurag Patel <anuragpatel.optnio@gmail.com>
2024-08-24 02:29:05 +0530

    feat: setup deployment configuration for demo env

--- config.ts
+++ config.ts
@@ -1,4 +1,5 @@
 export const Servers = {
   LiveServer: process.env.API_SERVER_BASE_URL || "https://34.213.177.104:3000/api/v1/",
   GoogleAPIkey: process.env.GOOGLE_API_KEY || "AIzaSyDOQ7N0NgZt8OFcioja-gHnX5hKjk-Su_8",
+  AppEnv: process.env.APP_ENV || 'development'
 };
