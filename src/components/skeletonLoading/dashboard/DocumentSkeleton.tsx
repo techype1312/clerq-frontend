562597d250290ac835b1dc928e7a2ca92ba61b5c
Aaryansh Bhardwaj <aaryanshsharma8@gmail.com>
2024-08-26 20:30:53 +0530

    feat: document skeleton

---
+++ src/components/skeletonLoading/dashboard/DocumentSkeleton.tsx
@@ -0,0 +1,30 @@
+import { TableBody, TableCell, TableRow } from "@/components/ui/table";
+import React from "react";
+import Skeleton from "react-loading-skeleton";
+
+const DocumentSkeleton = ({ duration }: { duration?: number }) => {
+  return (
+    <TableBody>
+      <TableRow>
+        <TableCell>
+          <Skeleton
+            height={25}
+            className="my-2"
+            duration={duration}
+            count={5}
+          />
+        </TableCell>
+        <TableCell>
+          <Skeleton
+            height={25}
+            className="my-2"
+            duration={duration}
+            count={5}
+          />
+        </TableCell>
+      </TableRow>
+    </TableBody>
+  );
+};
+
+export default DocumentSkeleton;
