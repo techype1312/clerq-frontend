6e5c7288f6a87fc3d3143db9a064e2632290be03
Aaryansh Bhardwaj <aaryanshsharma8@gmail.com>
2024-08-26 20:29:11 +0530

    feat: skeleton setup

--- src/app/dashboard/(verified)/layout.tsx
+++ src/app/dashboard/(verified)/layout.tsx
@@ -1,6 +1,7 @@
 import { DashboardLayout } from "@/components/dashboard/DashboardLayout";
 import { HelpCenter } from "@/components/help";
 import { CompanySessionProvider } from "@/context/CompanySession";
+import { SkeletonTheme } from "react-loading-skeleton";
 
 export default async function layout({
   children,
@@ -8,9 +9,15 @@ export default async function layout({
   children: React.ReactNode;
 }>) {
   return (
+    <SkeletonTheme 
+    // highlightColor="#5266EB"
+    highlightColor="#d3d3d3"
+    // highlightColor="#ebebeb"
+    >
     <CompanySessionProvider>
       <DashboardLayout>{children}</DashboardLayout>
       <HelpCenter />
     </CompanySessionProvider>
+    </SkeletonTheme>
   );
 }
