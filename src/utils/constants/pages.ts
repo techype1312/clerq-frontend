2969ab29ea7ea1b9e2811331ed4e3791b3a88396
Aaryansh Bhardwaj <aaryanshsharma8@gmail.com>
2024-09-03 00:37:44 +0530

    feat: pages and transactions mock data const

---
+++ src/utils/constants/pages.ts
@@ -0,0 +1,46 @@
+export const pages = [
+  {
+    name: "Overview",
+    pathname: "/dashboard",
+  },
+  {
+    name: "Transactions",
+    pathname: "/dashboard/transactions",
+  },
+  {
+    name: "Income statement",
+    pathname: "/dashboard/income-statement",
+  },
+  {
+    name: "Balance sheet",
+    pathname: "/dashboard/balance-sheet",
+  },
+  {
+    name: "Documents",
+    pathname: "/dashboard/documents",
+  },
+  {
+    name: "Settings",
+    pathname: "/dashboard/settings",
+  },
+  {
+    name: "Bank accounts",
+    pathname: "/dashboard/bank-accounts",
+  },
+  {
+    name: "Team",
+    pathname: "/dashboard/team",
+  },
+  {
+    name: "Profile",
+    pathname: "/dashboard/profile",
+  },
+  {
+    name: "Company security",
+    pathname: "/dashboard/company-security",
+  },
+  {
+    name: "Security",
+    pathname: "/dashboard/security",
+  },
+];
